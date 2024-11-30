82d11ab93d6b8efbcb3e22ab10bb9941a8241dcc
Rafsan12 <rafsan.web.2022@gmail.com>
2024-11-30 23:04:44 +0600

    login field create

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,18 @@
+import { useForm } from "react-hook-form";
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit(submitForm)}
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+    ></form>
+  );
+}
