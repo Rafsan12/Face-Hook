b96bf4f12a4fad3d0c238fbccf5738c5cf1d67c8
Rafsan12 <rafsan.web.2022@gmail.com>
2024-12-02 08:12:33 +0600

    useAxious auth complete

--- src/hooks/UseAxios.js
+++ src/hooks/UseAxios.js
@@ -1,11 +1,14 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { api } from "../api";
 import { useAuth } from "./Useauth";
 
 const useAxios = () => {
   const { auth, setAuth } = useAuth();
+
   useEffect(() => {
-    api.interceptors.request.use(
+    // Request interceptor
+    const requestIntercept = api.interceptors.request.use(
       (config) => {
         const authToken = auth?.authToken;
         if (authToken) {
@@ -15,5 +18,46 @@ const useAxios = () => {
       },
       (error) => Promise.reject(error)
     );
-  }, []);
+
+    // Response interceptor
+    const responseIntercept = api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+
+          try {
+            const refreshToken = auth?.refreshToken;
+            const response = await axios.post(
+              `${import.meta.env.VITE_SERVER_BASE_URL}/auth/refresh-token`,
+              { refreshToken }
+            );
+
+            const { token } = response.data;
+            console.log(`New Token: ${token}`);
+            setAuth({ ...auth, authToken: token });
+
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return axios(originalRequest);
+          } catch (error) {
+            throw new error();
+          }
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      // Cleanup interceptors
+      api.interceptors.request.eject(requestIntercept);
+      api.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth.authToken]);
+
+  return { api };
 };
+
+export default useAxios;
