a741267ed4943bae2e76c43598b1c88f6df02244
Rafsan12 <rafsan.web.2022@gmail.com>
2024-12-03 08:12:41 +0600

    profile bio show

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -1,7 +1,21 @@
+import EditIcon from "../../assets/icons/edit.svg";
+import UseProfile from "../../hooks/UseProfile";
+
 export default function ProfileBio() {
+  const { state } = UseProfile();
   return (
     <>
-      <h1>This is Bio</h1>
+      <div className="mt-4 flex items-start gap-2 lg:mt-6">
+        <div className="flex-1">
+          <p className="leading-[188%] text-gray-400 lg:text-lg">
+            {state?.user?.bio}
+          </p>
+        </div>
+        {/* <!-- Edit Bio button. The Above bio will be editable when clicking on the button --> */}
+        <button className="flex-center h-7 w-7 rounded-full">
+          <img src={EditIcon} alt="Edit" />
+        </button>
+      </div>
     </>
   );
 }
